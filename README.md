a1cf1c66d49d8cfb5fd494053f05275f627726bc
DasLampe <daslampe@lano-crew.org>
2018-07-11 19:11:26 +0200

    Rewrite for stretch

--- README.md
+++ README.md
@@ -1,15 +1,42 @@
 # Bundlewrap php-fpm
-Install php7.0-fpm or php5-fpm on Debian Jessie.
+Install php7.0-fpm on Debian Stretch
 
 __Don't use this for production! It's still work in progress!__
 
-## Options
-```json
-'php-fpm': {
-    'version': 7,
-    'modules': {
-        'php7.0-gd': True,
-        'php7.0-mysql': False,
+## Metadata
+```python
+    'php-fpm': {
+        'pools': {
+            'pool_name': {
+                'user': 'www-data',
+                'group': 'www-data',
+                'mode': '0666',
+                'pm': {
+                    'mode': 'dynamic',
+                    'max_children': '5',
+                    'start_server': '2',
+                    'min_spare_server': '1',
+                    'max_spare_server': '3',
+                    'proccess_idle_timeout': '10',
+                    'max_request': '0',
+                }
+
+                'access_log': False,
+                'access_format': '%R - %u %t \"%m %r\" %s',
+
+                'slow_log': '',
+
+                'disable_functions': False,
+
+                'php_admin_value': {
+                    'name': 'value',
+                },
+                'php_admin_flag': {
+                    'flag': 'value',
+                },
+            },
+        },
     },
-}
 ```
+
+Option disable_functions, disable ```shell_exex, system, exec, proc_open, popen, passthru, stream_socket_server, dl, phpinfo, posix_* apache_child_terminate apache_setenv, virtual```.
--- items.py
+++ items.py
@@ -1,70 +1,28 @@
+pkg_apt = {
+    'php-fpm': {},
+}
+svc_systemv = {
+    'php7.0-fpm': {
+        'needs': [
+            'pkg_apt:php-fpm',
+        ],
+    },
+}
+
 files = {}
-actions = {}
-pkg_apt = {}
 
-if node.metadata.get('php-fpm', False):
-    version = node.metadata['php-fpm'].get('version', '7')
-
-    # Use dotdebt for jessie and php7
-    if version == 7 and node.os_version[0] == 8:
-        actions['dotdeb_trustkey'] = {
-            'command': "wget https://www.dotdeb.org/dotdeb.gpg && apt-key add dotdeb.gpg",
-            'triggered': True,
-            'triggers': {
-                'action:force_update_apt_cache',
-            }
-        }
-        files['/etc/apt/sources.list.d/dotdeb.list'] = {
-            'content': "deb http://packages.dotdeb.org jessie all\n"\
-                       "deb-src http://packages.dotdeb.org jessie all",
-            'mode': '0660',
-            'owner': 'root',
-            'group': 'root',
-            'triggers': {
-                'action:dotdeb_trustkey',
-            },
-            'needed_by': {
-                'pkg_apt:',
-            }
-        }
-
-    #Install/Uninstall modules
-    for module,status in node.metadata['php-fpm'].get('modules', {}).items():
-        pkg_apt[module] = {'installed': status}
-
-    #Install PHP Version
-    if version == 7:
-        svc_systemd = {
-            "php7.0-fpm": {
-                'needs': ['pkg_apt:php7.0-fpm'],
-            }
-        }
-        pkg_apt = {
-            'php7.0-fpm': {
-                'installed': True,
-            }
-        }
-        files['/etc/php/7.0/fpm/pool.d/www.conf'] = {
-            'source': 'etc/php/7.0/fpm/pool.d/www.conf',
-            'triggers': [
-                'svc_systemd:php7.0-fpm:restart',
-            ]
-        }
-
-    if version == 5:
-        svc_systemd = {
-            "php5-fpm": {
-                'needs': ['pkg_apt:php5-fpm'],
-            }
-        }
-        pkg_apt = {
-            'php5-fpm': {
-                'installed': True,
-            }
-        }
-        files['/etc/php5/fpm/pool.d/www.conf'] = {
-            'source': 'etc/php5/fpm/pool.d/www.conf',
-            'triggers': [
-                'svc_systemd:php5-fpm:restart',
-            ]
-        }
+for pool_name,pool in node.metadata.get('php-fpm', {}).get('pools', {}).items():
+    files['/etc/php/7.0/fpm/pool.d/{}.conf'.format(pool_name)] = {
+        'source': 'etc/php/7.0/fpm/pool.d/pool.conf',
+        'content_type': 'mako',
+        'owner': 'root',
+        'group': 'root',
+        'mode': '0644',
+        'context': {
+            'pool_name': pool_name,
+            'pool': pool,
+        },
+        'triggers': [
+            'svc_systemv:php7.0-fpm:reload',
+        ],
+    }
